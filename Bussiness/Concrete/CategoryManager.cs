90c012500a712e0850d60ce1b84cf699b35c561f
Nergis Ketenci <33528338+NrgsK@users.noreply.github.com>
2023-11-16 13:49:17 +0300

    changes

---
+++ Bussiness/Concrete/CategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Bussiness.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Concrete
+{
+    public class CategoryManager : ICategoryService
+    {
+        ICategoryDal _categoryDal;
+        public CategoryManager(ICategoryDal categoryDal)
+        {
+            //Constructor
+            _categoryDal = categoryDal;
+        }
+        public List<Category> GetAll()
+        {
+            //İş kodları
+            return _categoryDal.GetAll();
+        }
+
+        public Category GetById(int categoryId)
+        {
+            return _categoryDal.Get(c=>c.CategoryId == categoryId);
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -8,17 +8,25 @@ internal class Program
     // Open Closed Principle : Yeni bir özellik ekliyorsan mevcuttaki hiçbir koda dokunamazsın.
     private static void Main(string[] args)
     {
+        //ProductTest();
+        //IoC container
+        //productManager.GetAll()
+        //productManager.GetAllByCategoryId(id)
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        foreach (var category in categoryManager.GetAll())
+        {
+            Console.WriteLine(category.CategoryName);
+        }
+
+    }
+
+    private static void ProductTest()
+    {
         ProductManager productManager = new ProductManager(new EfProductDal());
 
         foreach (var product in productManager.GetAllByUnitPrice(10, 100))
         {
             Console.WriteLine(product.ProductName);
         }
-        //productManager.GetAll()
-        //productManager.GetAllByCategoryId(id)
-
-
-
-        Console.ReadKey();
     }
 }
